5cda270f768db9d15c4627a11bdf311ea14fdb78
harshjha1121 <harshpin9@gmail.com>
2025-11-05 15:12:14 +0530

    Create app.js

---
+++ pgsql/app.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>🚀 React + Docker Multi-Stage Build</h1>
+        <p>This app is running inside a Docker container using Nginx!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
